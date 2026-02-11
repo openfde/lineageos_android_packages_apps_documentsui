5e7d335f5bb9f9d72e4880100ffeeb8eed851c97
xudingqiang <xudingqiang520@126.com>
2026-02-11 11:17:07 +0800

    Fix the issue where the status bar at the top of the title bar is not hidden in full-screen mode

--- src/com/android/documentsui/BaseActivity.java
+++ src/com/android/documentsui/BaseActivity.java
@@ -749,21 +749,21 @@ public abstract class BaseActivity
     }
 
     private void setContainer() {
-        View root = findViewById(R.id.coordinator_layout);
-        root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        root.setOnApplyWindowInsetsListener((v, insets) -> {
-            root.setPadding(insets.getSystemWindowInsetLeft(),
-                    insets.getSystemWindowInsetTop(), insets.getSystemWindowInsetRight(), 0);
+        // View root = findViewById(R.id.coordinator_layout);
+        // root.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        //         | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+        // root.setOnApplyWindowInsetsListener((v, insets) -> {
+        //     root.setPadding(insets.getSystemWindowInsetLeft(),
+        //             insets.getSystemWindowInsetTop(), insets.getSystemWindowInsetRight(), 0);
 
-            View saveContainer = findViewById(R.id.container_save);
-            saveContainer.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
+        //     View saveContainer = findViewById(R.id.container_save);
+        //     saveContainer.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
 
-            View rootsContainer = findViewById(R.id.container_roots);
-            rootsContainer.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
+        //     View rootsContainer = findViewById(R.id.container_roots);
+        //     rootsContainer.setPadding(0, 0, 0, insets.getSystemWindowInsetBottom());
 
-            return insets.consumeSystemWindowInsets();
-        });
+        //     return insets.consumeSystemWindowInsets();
+        // });
 
         getWindow().setNavigationBarDividerColor(Color.TRANSPARENT);
         if (Build.VERSION.SDK_INT >= 29) {
